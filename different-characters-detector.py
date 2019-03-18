b9d37840385823e15c18a13111ac7f80e0f1f94d
Tarcísio Fonseca <46990717+tarcisiofonseca@users.noreply.github.com>
2019-03-18 11:06:18 -0300

    Update different-characters-detector.py

--- different-characters-detector.py
+++ different-characters-detector.py
@@ -1,5 +1,3 @@
-import itertools
-
 character1 = []
 character2 = []
 list1 = []
@@ -10,21 +8,27 @@ s = 0
 a = "Insert your string here, but keeping quotation marks"
 b = "Insert your another string here, keeping quotation marks too"
 
-a = " ".join(a)
 a = a.split(" ")
+a = " ".join(a)
 
-b = " ".join(b)
 b = b.split(" ")
+b = " ".join(b)
 
 if a == b:
-    print("All characters are the same")
+    print("All characters are the same.")
 else:
-    for (character1,character2) in zip(a,b):
+    for (character1, character2) in zip(a, b):
         s = s+1
-        if character1!=character2:
+        if character1 != character2:
             g = s
             list1.append(character1)
             list2.append(character2)
             list3.append(g)
 
-print("In position {}, the characters {} and {} are, respectively, different".format(list3,list1,list2))
+for p in range(0, len(list3)):
+    print("In position", list3[p], ", the characters", list1[p], " and ", list2[p], "are, respectively, differents.")
+if len(a) > len(b):
+    print("\nThe first string have more characters than the second string. The text left over is:", a[len(b):])
+if len(b) > len(a):
+    print("\nThe second string have more characters than the first string. The text left over is:", b[len(a):])
+    
